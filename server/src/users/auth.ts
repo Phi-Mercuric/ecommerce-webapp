94a8820c5ec4a4091847ef74ffeca6b89db017b9
PhiYerion <PhiYerion@protonmail.com>
2023-07-20 14:53:52 -0400

    Json web token + authentication fix + query fix

--- server/src/users/auth.ts
+++ server/src/users/auth.ts
@@ -3,6 +3,7 @@ import { authSchema } from '../models/user.js';
 import { db } from '../db.js';
 import * as argon2 from 'argon2';
 import debug from 'debug';
+import jwt from 'jsonwebtoken';
 
 const dbg = debug('server:users');
 const router = Router();
@@ -21,19 +22,29 @@ router.post('/auth', async (req, res) => {
   }
 
   // --- Database ---
-  const { inpUsername: username, password } = req.body;
+  const { username, password } = req.body;
 
   // - Username/Email -
   // "username" is what the user entered, it could be either an email or a username (but user is prompted for username)
+  const test = 'reidbandy';
   const dbRes = await db.query('SELECT password FROM users WHERE username = $1 OR email = $1', [username]);
+
   if (dbRes.rows.length != 1) {
     dbg("\tEmail doesn't exist\n}")
+    dbg(dbRes)
     return res.status(472).send('Username or email doesn\'t exist');
-  }
-  if (!await argon2.verify(dbRes.rows[0].password, password)) {
+  } else if (!await argon2.verify(dbRes.rows[0].password, password)) {
     dbg("\tWrong password\n}")
     return res.status(400).send('Wrong password');
   }
+
+  // --- JWT ---
+  jwt.sign({ _id: dbRes.rows[0]._id }, 'replace with env var', { expiresIn: '1h' }, (err, token) => {
+    if (err)
+      dbg("\tJWT error: ", err, "\n}") // replace with proper error handling
+    else
+      res.send(token)
+  });
 });
 
 export default router;
